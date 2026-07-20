f5459c603b9997e842fc4403e0c0f43fd6d799af
summohit <68390827+summohit@users.noreply.github.com>
2026-07-20 14:56:17 +0530

    feat: increase all upload limits to 50MB

--- modules/case-studies/case-study.upload.js
+++ modules/case-studies/case-study.upload.js
@@ -5,7 +5,7 @@ const allowedTypes = ['image/jpeg', 'image/png', 'image/webp', 'image/svg+xml'];
 
 const upload = multer({
   storage,
-  limits: { fileSize: 8 * 1024 * 1024 },
+  limits: { fileSize: 50 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (allowedTypes.includes(file.mimetype)) {
       cb(null, true);
